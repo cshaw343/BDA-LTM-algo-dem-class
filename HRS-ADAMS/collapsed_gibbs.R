8f9a9affba2bf0f993951944dd8c8a5e41e9b9c0
Crystal Shaw <c.shaw@ucla.edu>
2019-12-29 16:36:24 -0800

    Update values of p0 and p1

--- HRS-ADAMS/collapsed_gibbs.R
+++ HRS-ADAMS/collapsed_gibbs.R
@@ -11,15 +11,33 @@ collapsed_gibbs <- function(fact_table_entry, source_priors,
   names(values_table) <- c("n00", "n01", "n10", "n11", "p0", "p1",
                            "p0_update", "p1_update")
   values_table[fact_table_entry$init_count] = 1
-  values_table["p0"] = source_priors[["beta_0"]]
-  values_table["p1"] = source_priors[["beta_1"]]
+  values_table["p0"] = source_priors[["beta0"]]
+  values_table["p1"] = source_priors[["beta1"]]
 
 
   #---- Begin sampling ----
   #Initial truth label
   tf <- fact_table_entry$t_f
+  oc <- fact_table_entry$Dementia
 
   #Update based on the claim
-  p_tf <-
+  values_table[paste0("p", tf, "_update")] <-
+    (values_table[paste0("p", tf)]*(
+      values_table[paste0("n", tf, oc)] - 1 +
+        source_priors[paste0("alpha", tf, oc)]))/
+    (values_table[paste0("n", tf, "1")] + values_table[paste0("n", tf, 0)] +
+       source_priors[paste0("alpha", tf, 1)] +
+       source_priors[paste0("alpha", tf, 0)])
+
+  values_table[paste0("p", 1 - tf, "_update")] <-
+    (values_table[paste0("p", 1 - tf)]*(
+      values_table[paste0("n", 1 - tf, oc)] +
+        source_priors[paste0("alpha", 1 - tf, oc)]))/
+    (values_table[paste0("n", 1 - tf, "1")] +
+       values_table[paste0("n", 1 - tf, 0)] +
+       source_priors[paste0("alpha", 1 - tf, 1)] +
+       source_priors[paste0("alpha", 1 - tf, 0)])
+
+  if(values_table[paste0("p", )])
 
 }
