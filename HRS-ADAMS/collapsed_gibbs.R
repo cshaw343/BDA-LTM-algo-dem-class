1a81d36f25ec247c833b3e6f43cd1d6567ae6cd8
Crystal Shaw <c.shaw@ucla.edu>
2019-12-31 22:44:11 -0800

    Feed in the entire source priors table to the function

--- HRS-ADAMS/collapsed_gibbs.R
+++ HRS-ADAMS/collapsed_gibbs.R
@@ -1,13 +1,16 @@
-collapsed_gibbs <- function(fact_table_entry, source_priors,
+collapsed_gibbs <- function(fact_table_entry, source_priors_table,
                             burn_in = 500, thinning = 1000, runs = 100000){
+
+  #---- Identify correct priors ----
+  source_priors <- source_priors_table[fact_table_entry["Source"], ]
 
   #---- Initializing values vector ----
   values_table <- vector(length = 8)
   names(values_table) <- c("n00", "n01", "n10", "n11", "p0", "p1",
                            "p0_update", "p1_update")
   values_table[fact_table_entry["init_count"]] = 1
-  values_table["p0"] = source_priors[["beta0"]]
-  values_table["p1"] = source_priors[["beta1"]]
+  values_table["p0"] = source_priors[, "beta0"]
+  values_table["p1"] = source_priors[, "beta1"]
 
   p_tf1 <- 0 #Probability that tf = 1
 
@@ -22,19 +25,19 @@ collapsed_gibbs <- function(fact_table_entry, source_priors,
     values_table[paste0("p", tf, "_update")] <-
       (values_table[paste0("p", tf)]*(
         values_table[paste0("n", tf, oc)] - 1 +
-          source_priors[paste0("alpha", tf, oc)]))/
+          source_priors[, paste0("alpha", tf, oc)]))/
       (values_table[paste0("n", tf, "1")] + values_table[paste0("n", tf, 0)] +
-         source_priors[paste0("alpha", tf, 1)] +
-         source_priors[paste0("alpha", tf, 0)])
+         source_priors[, paste0("alpha", tf, 1)] +
+         source_priors[, paste0("alpha", tf, 0)])
 
     values_table[paste0("p", 1 - tf, "_update")] <-
       (values_table[paste0("p", 1 - tf)]*(
         values_table[paste0("n", 1 - tf, oc)] +
-          source_priors[paste0("alpha", 1 - tf, oc)]))/
+          source_priors[, paste0("alpha", 1 - tf, oc)]))/
       (values_table[paste0("n", 1 - tf, "1")] +
          values_table[paste0("n", 1 - tf, 0)] +
-         source_priors[paste0("alpha", 1 - tf, 1)] +
-         source_priors[paste0("alpha", 1 - tf, 0)])
+         source_priors[, paste0("alpha", 1 - tf, 1)] +
+         source_priors[, paste0("alpha", 1 - tf, 0)])
 
     #Calculate if truth label needs to change
     draw_prob <- values_table[paste0("p", 1 - tf, "_update")]/
