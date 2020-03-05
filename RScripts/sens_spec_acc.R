18afee7ef897aff6068f136052321afc01f27eb8
cshawsome <c.shaw@ucla.edu>
2020-03-04 22:13:00 -0800

    Added accuracy metric

--- RScripts/sens_spec_acc.R
+++ RScripts/sens_spec_acc.R
@@ -1,4 +1,4 @@
-sens_spec <- function(test, gold){
+sens_spec_acc <- function(test, gold){
   #unlist so that we can compare two vectors
   if(length(dim(test)) != 0){
     test <- as.vector(test)
@@ -13,7 +13,8 @@ sens_spec <- function(test, gold){
   match <- rowSums(matrix)
   sens <- sum(match == 2)/sum(matrix[, "gold"])
   spec <- sum(match == 0)/(nrow(matrix) - sum(matrix[, "gold"]))
+  acc <- (sum(match == 2) + sum(match == 0))/nrow(matrix)
 
-  return(c("sensitivity" = sens, "specificity" = spec))
+  return(c("sensitivity" = sens, "specificity" = spec, "accuracy" = acc))
 }
 
